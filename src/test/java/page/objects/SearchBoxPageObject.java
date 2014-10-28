8bec3f703d139f876ebca3c944e3210cc3a63d1d
Serhii TANCHENKO <serg.tanchenko@gmail.com>
2014-10-29 01:53:30 +0200

    simple test was added

---
+++ src/test/java/page/objects/SearchBoxPageObject.java
@@ -0,0 +1,7 @@
+package page.objects;
+
+/**
+ * Created by Serg on 29.10.2014.
+ */
+public class SearchBoxPageObject {
+}
---
+++ src/test/java/steps/NavigationSteps.java
@@ -0,0 +1,7 @@
+package steps;
+
+/**
+ * Created by Serg on 29.10.2014.
+ */
+public class NavigationSteps {
+}
